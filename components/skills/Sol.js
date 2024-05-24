f960ac59fe6b2d37938c2359f6b58993a1e551e9
ILYAS EL BAKKALI <i.elbakkali@ife-france.fr>
2024-05-24 17:28:13 +0200

    adapt1

---
+++ components/skills/Sol.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Sol() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <g id="zincerie">
+        <polygon className="cls-1" points="496.58 277 258.9 409.24 21.22 277 258.9 144.77 496.58 277" />
+        <line className="cls-1" x1="141.78" y1="211.78" x2="377.8" y2="343.09" />
+        <line className="cls-1" x1="140.63" y1="343.43" x2="378.31" y2="211.2" />
+        <polygon className="cls-1" points="378.31 177.57 258.9 243.16 141.78 177.15 258.9 111.14 378.31 177.57" />
+      </g>
+    </svg>
+  );
+}
